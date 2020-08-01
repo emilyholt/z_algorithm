c77f9b7aa302e55bb9e845ad73c2eb0c3fec8bb3
Emily Holt <emily.holt@dartmouth.edu>
2020-08-01 19:14:09 -0400

    Z Boxes + Right endpoints

---
+++ z_algo.py
@@ -0,0 +1,49 @@
+
+def find_z_box(input_string):
+    z_box = [0] * len(input_string)
+    
+    left_endpoints = [0] * len(input_string)
+    
+    for current_index in range(1, len(input_string)):
+        pattern_index = 0
+        while pattern_index + current_index < len(input_string)\
+         and input_string[pattern_index] == input_string[pattern_index + current_index]:
+            pattern_index += 1
+        z_box[current_index] = pattern_index
+    return z_box
+
+def find_right_endpoints(input_string, z_box):
+    right_endpoints = [0] * len(input_string)
+    rightmost_endpoint = 0
+
+    for current_index in range(1, len(input_string)):
+        if z_box[current_index] is not 0:
+            candidate_endpoint = current_index + z_box[current_index]
+            
+            if candidate_endpoint > rightmost_endpoint:
+                rightmost_endpoint = candidate_endpoint
+
+        right_endpoints[current_index] = rightmost_endpoint
+
+    return right_endpoints
+
+
+def test():
+    z_input = "AABAABCAXAABAABCYAAAAB"
+    # z_input = "AABAAB"
+    z_box = find_z_box(z_input)
+    right_endpoints = find_right_endpoints(z_input, z_box)
+    z_box[0] = 0
+
+    test_string = [str(x) + " " for x in z_input]
+    z_box = [str(x) + " " if x < 10 else str(x) for x in z_box]
+    right_endpoints = [str(x) + " " if x < 10 else str(x) for x in right_endpoints]
+    # left_endpoints = [str(x) + " " if x < 10 else str(x) for x in left_endpoints]
+
+    print(f"Test string:                {test_string}")
+    print(f"Resulting z-box:            {z_box}")
+    print(f"Resulting right_endpoints:  {right_endpoints}")
+    # print(f"Resulting left_endpoints:   {left_endpoints}")
+
+if __name__ == '__main__':
+    test()
