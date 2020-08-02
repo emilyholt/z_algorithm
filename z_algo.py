d0ccc60442b16ea1537cab27cfa158982abbe093
Emily Holt <emily.holt@dartmouth.edu>
2020-08-02 15:29:55 -0400

    Input from cmd line

--- z_algo.py
+++ z_algo.py
@@ -1,4 +1,4 @@
-
+import argparse
 from colorama import init, Fore, Back, Style
 
 init()
@@ -83,6 +83,7 @@ def test():
     left_endpoints = find_left_endpoints(z_input, z_box, right_endpoints)
     z_box[0] = 0
 
+    # Formatting for printing
     test_string = [str(x) + " " for x in z_input]
     z_box = [str(x) + " " if x < 10 else str(x) for x in z_box]
     right_endpoints = [str(x) + " " if x < 10 else str(x) for x in right_endpoints]
@@ -94,5 +95,32 @@ def test():
     print(f"Resulting right_endpoints:  {right_endpoints}")
     print(f"Resulting left_endpoints:   {left_endpoints}")
 
+def run(pattern, input_string):
+    z_input = pattern + "$" + input_string
+    z_box = find_z_box(z_input)
+    right_endpoints = find_right_endpoints(z_input, z_box)
+    left_endpoints = find_left_endpoints(z_input, z_box, right_endpoints)
+    z_box[0] = 0
+
+    # Formatting for printing
+    test_string = [str(x) + " " for x in z_input]
+    z_box = [str(x) + " " if x < 10 else str(x) for x in z_box]
+    right_endpoints = [str(x) + " " if x < 10 else str(x) for x in right_endpoints]
+    left_endpoints = [str(x) + " " if x < 10 else str(x) for x in left_endpoints]
+    
+    print(f"\n\n\tResults\n........................\n")
+    print(f"Test string:                {test_string}")
+    print(f"Resulting z-box:            {z_box}")
+    print(f"Resulting right_endpoints:  {right_endpoints}")
+    print(f"Resulting left_endpoints:   {left_endpoints}")
+
 if __name__ == '__main__':
-    test()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pattern", help="pattern to search for")
+    parser.add_argument("input_string", help="pattern to search for")
+    args = parser.parse_args()
+    run(args.pattern, args.input_string)
+    # test()
+
+
+
