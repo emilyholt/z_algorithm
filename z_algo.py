89520aafbda21cdb575559e35075d329f9fb73ee
Emily Holt <emily.holt@dartmouth.edu>
2020-08-02 15:23:49 -0400

    Pretty-printed output

--- z_algo.py
+++ z_algo.py
@@ -1,5 +1,33 @@
 
-def find_z_box(input_string):
+from colorama import init, Fore, Back, Style
+
+init()
+
+def pretty_print_zbox(input_string, pattern_index, current_index):
+    pattern_string = ""
+    intermediate_string = ""
+    z_string = ""
+    end_string = ""
+
+    # Separate the pattern & z-boxes into strings
+    for i, elem in enumerate(input_string):
+        if i <= pattern_index:
+            pattern_string = pattern_string + str(elem) + " "
+        elif i < current_index:
+            intermediate_string = intermediate_string + str(elem) + " "
+        elif i <= current_index + pattern_index:
+            z_string = z_string + str(elem) + " "
+        else:
+            end_string = end_string + str(elem) + " "
+
+    # Pretty printing of our color-coded z-box
+    print(Fore.GREEN + pattern_string
+        + Fore.RESET + intermediate_string
+        + Fore.BLUE + z_string
+        + Fore.RESET + end_string
+        , end='\n')
+
+def find_z_box(input_string, descriptive=True):
     z_box = [0] * len(input_string)
     
     left_endpoints = [0] * len(input_string)
@@ -8,7 +36,14 @@ def find_z_box(input_string):
         pattern_index = 0
         while pattern_index + current_index < len(input_string)\
          and input_string[pattern_index] == input_string[pattern_index + current_index]:
+            
+            if descriptive:
+                print(f"\nCurrent Index: {current_index}")
+                print(f"Found new match: input[{current_index + pattern_index}] = {input_string[current_index + pattern_index]} = input[{pattern_index}] = {input_string[pattern_index]}")
+                pretty_print_zbox(input_string, pattern_index, current_index)
+
             pattern_index += 1
+
         z_box[current_index] = pattern_index
     return z_box
 
@@ -33,7 +68,6 @@ def find_left_endpoints(input_string, z_box, right_endpoints):
 
     for current_index in range(1, len(input_string)):
         if z_box[current_index] is not 0:
-            # print(f"At Index [{current_index}]; z-box = {z_box[current_index]}; right_endpoints = {right_endpoints[current_index]}")
             if z_box[current_index] + current_index - 1 >= right_endpoints[current_index]:
                 leftmost_endpoint = current_index
 
@@ -54,6 +88,7 @@ def test():
     right_endpoints = [str(x) + " " if x < 10 else str(x) for x in right_endpoints]
     left_endpoints = [str(x) + " " if x < 10 else str(x) for x in left_endpoints]
 
+    print(f"\n\n\tResults\n........................\n")
     print(f"Test string:                {test_string}")
     print(f"Resulting z-box:            {z_box}")
     print(f"Resulting right_endpoints:  {right_endpoints}")
